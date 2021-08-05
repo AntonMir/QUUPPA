d740433b1e57ebc72d905d5a04defde9ca421417
Черноусов Антон Сергеевич <chernousov@ITL-NPP.local>
2021-08-05 18:03:30 +0300

    05.08.2021

--- back-end/app.js
+++ back-end/app.js
@@ -1,105 +1,96 @@
 /**
  * Required packeges
  */
- const express = require('express')
- const app = express()
- const WSServer = require('express-ws')(app)
- const http = require('http');
- const cors = require('cors')
- const fs = require('fs')
- const path = require('path')
-  
- 
- /** 
-  * App variables
-  */
- const PORT = process.env.PORT || 5000
- let devicePosition = '';
- const interval = 1000;
- 
- 
- /**
-  * App Configuration
-  */
- app.use(cors())
- app.use(express.json())
- 
- 
- /**
-  * Server init
-  */
-  const server = app.listen(PORT, () => {
-     console.log(`Listening to requests on http://localhost:${PORT}`);
- })
- 
- app.ws('/', (ws, req) => {
-     console.log('---', 'ПОДКЛЮЧЕНИЕ УСТАНОВЛЕНО');
- })
- 
- 
- // app.ws('/', (ws, req) => {
- //     ws.on('message', (msg) => {
- //         msg = JSON.parse(msg)
- //         switch (msg.method) {
- //             case "connection":
- //                 connectionHandler(ws, msg)
- //                 break
- //             case "draw":
- //                 broadcastConnection(ws, msg)
- //                 break
- //         }
- //     })
- // })
- 
- // app.post('/image', (req, res) => {
- //     try {
- //         const data = req.body.img.replace(`data:image/png;base64,`, '')
- //         fs.writeFileSync(path.resolve(__dirname, 'files', `${req.query.id}.jpg`), data, 'base64')
- //         return res.status(200).json({message: "Загружено"})
- //     } catch (e) {
- //         console.log(e)
- //         return res.status(500).json('error')
- //     }
- // })
- // app.get('/image', (req, res) => {
- //     try {
- //         const file = fs.readFileSync(path.resolve(__dirname, 'files', `${req.query.id}.jpg`))
- //         const data = `data:image/png;base64,` + file.toString('base64')
- //         res.json(data)
- //     } catch (e) {
- //         console.log(e)
- //         return res.status(500).json('error')
- //     }
- // })
- 
- 
- /**
-  * get obj from TomCat
-  */
- function getDevicesPosition(){
-     http.get('http:/ips.itelma.su:8080/qpe/getTagData?format=defaultLocation&humanReadable=true&maxAge=5000', (resp) => {
-         let data = '';
-         
-         // A chunk of data has been received.
-         resp.on('data', (chunk) => {
-             data += chunk;
-         });
-         
-         // The whole response has been received. Print out the result.
-         resp.on('end', () => {
-             devicePosition = JSON.parse(data);
-             let tagPos = 0;
-             console.log('_______________________________________')
-             devicePosition.tags.map(tag => {
-                 console.log(
-                     'TAG:', tagPos++,
-                     '| x = ', tag.location[0],
-                     '| y = ', tag.location[1],
-                     '| z = ', tag.location[2]
-                 )
-             })
-         });
-     });
- }
- 
- setInterval(getDevicesPosition, interval);
+const express = require('express')
+const http = require('http');
+// const fs = require('fs')
+// const path = require('path')
+
+/** 
+ * App variables
+ */
+const app = express()
+const PORT = process.env.PORT || 5000
+let devicePosition = '';
+const interval = 1000;
+
+/**
+ * App Configuration
+ */
+app.use(express.json())
+
+/**
+ * Server init
+ */
+const server = app.listen(PORT, () => {
+    console.log(`Listening to requests on http://localhost:${PORT}`);
+})
+
+/**
+ * WebSocket connect settings
+ */
+const io = require('socket.io')(server, {
+	cors: {
+		origin: "http://localhost:3000",
+		methods: ["GET","POST"],
+		credentials: true
+	}
+});
+
+/**
+ * WebSocket connect init
+ * Send data to client
+ */
+io.once('connection', (socket) => {
+    console.log('WS: Клиент подключился')
+    let count = 0;
+    setInterval(sendDevicePosition, interval);
+})
+
+/**
+ * get obj from TomCat
+ */
+function getDevicesPosition(){
+    http.get('http:/ips.itelma.su:8080/qpe/getTagData?format=defaultLocation&humanReadable=true&maxAge=5000', (resp) => {
+        let data = '';
+        resp.on('data', (chunk) => {
+            data += chunk;
+        });
+        resp.on('end', () => {
+            devicePosition = JSON.parse(data); 
+            console.log('---','Devices: ', devicePosition?.tags.length);          
+        });
+    });
+}
+
+function sendDevicePosition() {
+    getDevicesPosition()
+    io.emit('transferTagData', devicePosition)
+}
+
+
+
+
+
+
+   
+// app.post('/image', (req, res) => {
+//     try {
+//         const data = req.body.img.replace(`data:image/png;base64,`, '')
+//         fs.writeFileSync(path.resolve(__dirname, 'files', `${req.query.id}.jpg`), data, 'base64')
+//         return res.status(200).json({message: "Загружено"})
+//     } catch (e) {
+//         console.log(e)
+//         return res.status(500).json('error')
+//     }
+// })
+// app.get('/image', (req, res) => {
+//     try {
+//         const file = fs.readFileSync(path.resolve(__dirname, 'files', `${req.query.id}.jpg`))
+//         const data = `data:image/png;base64,` + file.toString('base64')
+//         res.json(data)
+//     } catch (e) {
+//         console.log(e)
+//         return res.status(500).json('error')
+//     }
+// })
